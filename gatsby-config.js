0681b6b78d97aa84969f0853dd420d8682195e48
Phan Phuoc Y <phanphuocy@gmail.com>
2020-10-11 15:02:12 +0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const path = require("path");
 
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
 module.exports = {
   siteMetadata: {
     title: "Home Owners Association",
@@ -27,10 +40,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-      },
+      options: contentfulConfig,
     },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,34 +4,12 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Img from "gatsby-image";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
 export default function Home() {
-  const data = useStaticQuery(graphql`
-    query {
-      contentfulAsset(contentful_id: { eq: "1oGFZ8nWAupdamJd0DOkV8" }) {
-        id
-        fluid {
-          ...GatsbyContentfulFluid_withWebp
-        }
-        description
-      }
-      contentfulHome(contentful_id: { eq: "4INJEtKpNVWj8YbwPMUUqO" }) {
-        id
-        body {
-          json
-        }
-        title
-      }
-    }
-  `);
-
-  const { title, body } = data.contentfulHome;
-  const { fluid, description } = data.contentfulAsset;
-
   return (
     <Layout>
       <Seo title="Welcome" description="A Home Owners Association Template" />
@@ -44,24 +22,9 @@ export default function Home() {
       >
         <Grid item xs={12}>
           <Typography variant="h3" color="textPrimary">
-            {title}
+            HELLO WORLD
           </Typography>
-        </Grid>
-        <Grid item xs={12} md={5}>
-          <Typography color="textPrimary" component="div">
-            {documentToReactComponents(body.json)}
-          </Typography>
-        </Grid>
-        <Grid item xs={false} md={1}></Grid>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={5} style={{ width: "100%", height: "50vh" }}>
-            <Img
-              fluid={fluid}
-              fadeIn
-              alt={description}
-              style={{ width: "100%", height: "100%" }}
-            ></Img>
-          </Paper>
+          <Button>Test Button</Button>
         </Grid>
       </Grid>
     </Layout>
